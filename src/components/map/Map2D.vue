8feae0c885957932a4ef7833095cb7dfff3a1b7a
Noe Sciaroni <sciaroni.noe99@gmail.com>
2026-03-26 20:35:21 +0100

    Modification au fichier map2D et map3D

--- src/components/map/Map2D.vue
+++ src/components/map/Map2D.vue
@@ -1,73 +1,82 @@
 <template>
-  <div class="map-placeholder">
-    <p>Zone Carte 2D (OpenLayers)</p>
-  </div>
+  <div ref="map2D" class="map2D"></div>
 </template>
 
+<script>
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
+import TileLayer from 'ol/layer/Tile.js';
+import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS.js';
+import Projection from 'ol/proj/Projection.js'
+import { getCenter } from 'ol/extent';
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4";
+
+export default {
+	data() {},
+	methods: {},
+	mounted() {
+        const parser = new WMTSCapabilities();
+
+        fetch('https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=fr')
+        .then(response => {
+        return response.text();
+        })
+        .then(text => {
+            const result = parser.read(text);
+
+            proj4.defs(
+                "EPSG:2056",
+                "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000" +
+                " +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs",
+            );
+            register(proj4);
+
+            const extent = [2496595, 1067184, 2837805, 1291771]
+
+            const projection = new Projection({
+                code: "EPSG:2056",
+                extent: extent,
+                units: "m"
+            })
+
+            new Map({
+                target: this.$refs.map2D,
+                layers: [
+                    new TileLayer({
+                        opacity: 1,
+                        source: new WMTS(optionsFromCapabilities(result, {
+                        layer: 'ch.swisstopo.swisssurface3d-reliefschattierung-multidirektional',
+                        matrixSet: 'EPSG:2056',
+                        })),
+                    })
+                ],
+                view: new View({
+                    projection: projection,
+                    extent: extent,
+                    center: getCenter(extent),
+                    zoom: 1,
+                }),
+                controls: [],
+            });
+        });
+    }
+}
+
+</script>
+
 <style scoped>
-.map-placeholder {
+
+.map2D {
   width: 100%;
   height: 100%;
-  background-color: #bbffc1; /* Verde chiaro */
+  background-color: #bbffc1;
   display: flex;
   align-items: center;
   justify-content: center;
   color: #2e7d32;
   font-weight: bold;
 }
+
 </style>
-
-<!-- 
-<template>
-  <div ref="mapElement" class="map-container"></div>
-</template>
-
-<script setup>
-import { onMounted, ref } from 'vue';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
-import WMTS, { optionsFromCapabilities } from 'ol/source/WMTS.js';
-import TileLayer from 'ol/layer/Tile.js';
-
-const mapElement = ref(null); // Référence au div HTML
-
-onMounted(async () => {
-  const capabilitiesUrl = 'https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=it';
-  
-  try {
-    const response = await fetch(capabilitiesUrl);
-    const text = await response.text();
-    const result = new WMTSCapabilities().read(text);
-
-    const options = optionsFromCapabilities(result, {
-      layer: 'ch.swisstopo.landeskarte-farbe-10',
-      matrixSet: 'EPSG:3857',
-    });
-
-    // On crée la carte OL
-    new Map({
-      target: mapElement.value, // On utilise la référence Vue
-      layers: [
-        new TileLayer({
-          source: new WMTS(options),
-        })
-      ],
-      view: new View({
-        center: [917900, 5906000],
-        zoom: 8,
-      }),
-    });
-  } catch (err) {
-    console.error("Erreur OpenLayers:", err);
-  }
-});
-</script>
-
-<style scoped>
-.map-container {
-  width: 100%;
-  height: 100%;
-  background-color: #e8f5e9;
-}
-</style> -->
--- src/components/map/Map3D.vue
+++ src/components/map/Map3D.vue
@@ -1,73 +1,68 @@
 <template>
-  <div class="map-placeholder">
-    <p>Zone Carte 2D (OpenLayers)</p>
-  </div>
+    <div id="view"></div>
 </template>
 
-<style scoped>
-.map-placeholder {
-  width: 100%;
-  height: 100%;
-  background-color: #ffbbbb; /* Verde chiaro */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #7d2e2e;
-  font-weight: bold;
-}
-</style>
+<script>
+import { MapControls } from "three/examples/jsm/controls/MapControls.js";
+import CoordinateSystem from "@giro3d/giro3d/core/geographic/CoordinateSystem.js";
+import Extent from "@giro3d/giro3d/core/geographic/Extent.js";
+import Instance from "@giro3d/giro3d/core/Instance.js";
+import ColorLayer from "@giro3d/giro3d/core/layer/ColorLayer.js";
+import Map from "@giro3d/giro3d/entities/Map.js";
+import WmtsSource from "@giro3d/giro3d/sources/WmtsSource.js";
+export default {
+	data() {},
+	methods: {},
+	mounted() {
+        const crs2056 = CoordinateSystem.register(
+        "EPSG:2056",
+        "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs",
+        );
 
-<!-- 
-<template>
-  <div ref="mapElement" class="map-container"></div>
-</template>
+        const extent = new Extent(
+            crs2056,
+            2496595, 2837805, 1067184, 1291771
+        );
 
-<script setup>
-import { onMounted, ref } from 'vue';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
-import WMTS, { optionsFromCapabilities } from 'ol/source/WMTS.js';
-import TileLayer from 'ol/layer/Tile.js';
+        const instance = new Instance({
+            target: 'view',
+            crs: crs2056,
+        });
 
-const mapElement = ref(null); // Référence au div HTML
+        const map = new Map({ extent });
 
-onMounted(async () => {
-  const capabilitiesUrl = 'https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=it';
-  
-  try {
-    const response = await fetch(capabilitiesUrl);
-    const text = await response.text();
-    const result = new WMTSCapabilities().read(text);
+        instance.add(map);
 
-    const options = optionsFromCapabilities(result, {
-      layer: 'ch.swisstopo.landeskarte-farbe-10',
-      matrixSet: 'EPSG:3857',
-    });
-
-    // On crée la carte OL
-    new Map({
-      target: mapElement.value, // On utilise la référence Vue
-      layers: [
-        new TileLayer({
-          source: new WMTS(options),
+        WmtsSource.fromCapabilities(
+        "https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=fr",
+        {
+            layer: "ch.swisstopo.swisssurface3d-reliefschattierung-multidirektional",
+            matrixSet: "EPSG:2056",
+        },
+        ).then((source) => {
+            map.addLayer(new ColorLayer({ name: "wmts", source }));
         })
-      ],
-      view: new View({
-        center: [917900, 5906000],
-        zoom: 8,
-      }),
-    });
-  } catch (err) {
-    console.error("Erreur OpenLayers:", err);
-  }
-});
+
+        const center = extent.centerAsVector2();
+        instance.view.camera.position.set(center.x, center.y - 1, 500000);
+
+        const controls = new MapControls(instance.view.camera, instance.domElement);
+        controls.target.set(center.x, center.y, 0);
+
+        instance.view.setControls(controls);
+    }
+}
 </script>
 
 <style scoped>
-.map-container {
+#view {
   width: 100%;
   height: 100%;
-  background-color: #e8f5e9;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000000;
+  font-weight: bold;
 }
-</style> -->
+</style>
