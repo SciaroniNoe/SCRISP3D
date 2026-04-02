36d778656cbe70332355f06e1f087bb30014ea63
Noe Sciaroni <sciaroni.noe99@gmail.com>
2026-04-02 11:10:28 +0200

    V_11h10

--- src/components/map/Map3D.vue
+++ src/components/map/Map3D.vue
@@ -1,69 +1,107 @@
 <template>
-    <div id="map3D"></div>
+  <div id="map3d" ref="map3d"></div>
 </template>
 
 <script>
-import { MapControls } from "three/examples/jsm/controls/MapControls.js";
-import CoordinateSystem from "@giro3d/giro3d/core/geographic/CoordinateSystem.js";
-import Extent from "@giro3d/giro3d/core/geographic/Extent.js";
-import Instance from "@giro3d/giro3d/core/Instance.js";
-import ColorLayer from "@giro3d/giro3d/core/layer/ColorLayer.js";
-import Map from "@giro3d/giro3d/entities/Map.js";
-import WmtsSource from "@giro3d/giro3d/sources/WmtsSource.js";
+import * as Cesium from 'cesium';
+import "cesium/Source/Widgets/widgets.css";
+import proj4 from 'proj4';
+
+window.CESIUM_BASE_URL = '/node_modules/cesium/Build/Cesium/';
 
 export default {
-	data() {},
-	methods: {},
-	mounted() {
-        const crs2056 = CoordinateSystem.register(
-        "EPSG:2056",
-        "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs",
-        );
+  name: 'map3D',
+  mounted() {
+    this.setupCesium();
+  },
+  methods: {
+    async setupCesium() {
+      const {
+        Viewer, CesiumTerrainProvider, ImageryLayer, UrlTemplateImageryProvider,
+        Rectangle, Cesium3DTileset, Cartesian3, Math: CesiumMath
+      } = Cesium;
 
-        const extent = new Extent(
-            crs2056,
-            2496595, 2837805, 1067184, 1291771
-        );
 
-        const instance = new Instance({
-            target: 'map3D',
-            crs: crs2056,
+      proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
+
+
+      const E_MN95 = 2600000;
+      const N_MN95 = 1200000;
+
+      // Conversione MN95 -> WGS84 [Longitudine, Latitudine]
+      const [longitude, latitude] = proj4("EPSG:2056", "EPSG:4326", [E_MN95, N_MN95]);
+      console.log(`Coordinate WGS84: Longitudine=${longitude}, Latitudine=${latitude}`);
+
+      const swissBounds = [5.9559, 45.8179, 10.4921, 47.8084];
+
+      try {
+        const viewer = new Viewer(this.$refs.map3d, {
+          animation: false,
+          timeline: false,
+          baseLayerPicker: false,
+          infoBox: false,
+          selectionIndicator: false,
+          fullscreenButton: false,
+          navigationHelpButton: false,
+          homeButton: false,
+          sceneModePicker: false,
+          geocoder: false,
+          terrainProvider: await CesiumTerrainProvider.fromUrl(
+            "https://3d.geo.admin.ch/ch.swisstopo.terrain.3d/v1/"
+          ),
+          baseLayer: new ImageryLayer(
+            new UrlTemplateImageryProvider({
+              // url: "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swisstlm3d-karte-farbe.3d/default/current/3857/{z}/{x}/{y}.jpeg",
+              url: "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/3857/{z}/{x}/{y}.jpeg",
+              minimumLevel: 8,
+              maximumLevel: 17,
+              rectangle: Rectangle.fromDegrees(...swissBounds),
+            })
+          ),
         });
 
-        const map = new Map({ extent });
+        const swissBuildings = await Cesium3DTileset.fromUrl(
+          "https://3d.geo.admin.ch/ch.swisstopo.swissbuildings3d.3d/v1/tileset.json"
+        );
+        viewer.scene.primitives.add(swissBuildings);
 
-        instance.add(map);
+        // --- CAMERA SETTINGS ---
+        const pitchGradi = -35; 
 
-        WmtsSource.fromCapabilities(
-        "https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=fr",
-        {
-            layer: "ch.swisstopo.swisssurface3d-reliefschattierung-multidirektional",
-            matrixSet: "EPSG:2056",
-        },
-        ).then((source) => {
-            map.addLayer(new ColorLayer({ name: "wmts", source }));
-        })
+        viewer.camera.setView({
+          destination: Cartesian3.fromDegrees(longitude, latitude, 2500), // 2500 metri di altitudine
+          orientation: {
+            heading: CesiumMath.toRadians(0.0),      // Direzione Nord
+            pitch: CesiumMath.toRadians(pitchGradi), // Inclinazione
+            roll: 0.0
+          },
+        });
 
-        const center = extent.centerAsVector2();
-        instance.view.camera.position.set(center.x, center.y - 1, 500000);
+        this.viewer = viewer;
 
-        const controls = new MapControls(instance.view.camera, instance.domElement);
-        controls.target.set(center.x, center.y, 0);
-
-        instance.view.setControls(controls);
+      } catch (error) {
+        console.error("Cesium setup error:", error);
+      }
     }
-}
+  },
+  beforeUnmount() {
+    if (this.viewer) {
+      this.viewer.destroy();
+    }
+  }
+};
 </script>
 
 <style scoped>
-#map3D {
+#map3d {
+  position: absolute;
   width: 100%;
   height: 100%;
-  background-color: #ffffff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #000000;
-  font-weight: bold;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
+
 </style>
